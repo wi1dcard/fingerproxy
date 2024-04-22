77b875657e911b8dbd151845410921a6131f5514
hellodword <46193371+hellodword@users.noreply.github.com>
2024-04-22 05:22:57 +0000

    Add missing priority in the HEADER frame

--- pkg/http2/server.go
+++ pkg/http2/server.go
@@ -1544,6 +1544,15 @@ func (sc *serverConn) processFrame(f Frame) error {
 				headers = append(headers, metadata.HeaderField(h))
 			}
 			md.HTTP2Frames.Headers = headers
+			if f.HasPriority() {
+				md.HTTP2Frames.Priorities = append(md.HTTP2Frames.Priorities,
+					metadata.Priority{
+						StreamId:  f.StreamID,
+						StreamDep: f.Priority.StreamDep,
+						Exclusive: f.Priority.Exclusive,
+						Weight:    f.Priority.Weight,
+					})
+			}
 		}
 		return sc.processHeaders(f)
 	case *WindowUpdateFrame:
